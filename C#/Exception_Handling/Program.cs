c5d78810968bf2d213995730e12ddfc98e595159
Tohid Yousefi <31246004+tohid-yousefi@users.noreply.github.com>
2022-11-14 21:21:23 +0300

    Upload Files

---
+++ C#/Exception_Handling/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num;
+                Console.Write("Enter a Integer Number: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Youre Integer Number is: " + num);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error is: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Continue Code...");
+            }
+        }
+    }
+}
