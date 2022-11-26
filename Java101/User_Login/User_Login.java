b6b5cc567319455363bc0ac7f1653b172c0fd7cd
Tohid Yousefi <31246004+tohid-yousefi@users.noreply.github.com>
2022-11-26 21:42:56 +0300

    Update User_Login.java

--- Java101/User_Login/User_Login.java
+++ Java101/User_Login/User_Login.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class User_Login {
     public static void main(String[] args) {
-        String username,password;
+        String username, password, reset_password;
 
         Scanner input = new Scanner(System.in);
         System.out.println("*********** User Login ***********");
@@ -20,6 +20,19 @@ public class User_Login {
             System.out.println("Giriş Başarılı.");
         }else if(username.equals("Tohid") && !password.equals("123456")){
             System.out.println("Şifreniz Doğru Değil!!!");
+            System.out.print("Şifrenizi sıfırlamak ister misiniz(Yes/No)?");
+            reset_password = input.nextLine();
+            if(reset_password.equals("Yes")){
+                System.out.print("Yeni şifrenizi giriniz: ");
+                password = input.nextLine();
+                if(password.equals("123456")){
+                    System.out.println("Şifre oluşturulamadı lütfen başka bir şifre giriniz.");
+                } else if (password.equals("Tohid")) {
+                    System.out.println("Kullanıcı adınız ile şifre aynı olamaz.");
+                }
+            }else{
+                System.out.println("Tekrar görüşmek üzere :)");
+            }
         }else if(!username.equals("Tohid") && password.equals("123456")){
             System.out.println("Kullanıcı Adınız Doğru Değil!!!");
         }else{
